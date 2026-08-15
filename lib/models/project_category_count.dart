4eefcfc140786dd4b484e3ba70a1b1ef1d82c7d4
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-15 21:13:24 +0300

    feat: Define `ProjectCategoryCount` model, mirroring from the webapp model of same name

---
+++ lib/models/project_category_count.dart
@@ -0,0 +1,16 @@
+class ProjectCategoryCount {
+  final String category;
+  final int count;
+
+  const ProjectCategoryCount({
+    required this.category,
+    required this.count,
+  });
+
+  factory ProjectCategoryCount.fromJson(Map<String, dynamic> json) {
+    return ProjectCategoryCount(
+      category: json['category'] as String,
+      count: (json['count'] as num).toInt(),
+    );
+  }
+}
