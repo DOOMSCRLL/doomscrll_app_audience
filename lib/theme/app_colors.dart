dbbfcb10fc5f0e824fbdfae7a4e67212a027b102
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-15 20:53:14 +0300

    feat: Add DOOMSCRLL theme colors

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const Color darkness = Color(0xFF121212);
+  static const Color lagoon = Color(0xFF3BD69C);
+  static const Color bloodmoon = Color(0xFFA31D2D);
+}
