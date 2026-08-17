bb0df92152a3d534e65c0191c87436c986d55bbe
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-17 21:35:13 +0300

    refactor: Use `TextButton.icon` constructor inside `CategoryAnchor`; update the text to use uppercase label; formatter changes

--- lib/views/home/widgets/category_anchor.dart
+++ lib/views/home/widgets/category_anchor.dart
@@ -1,42 +1,23 @@
 import "package:doomscrll_app_audience/theme/app_colors.dart";
+import "package:doomscrll_app_audience/utils/string_extensions.dart";
 import "package:doomscrll_app_audience/views/common/doomscrll_icon.dart";
 import "package:flutter/material.dart";
 
 class CategoryAnchor extends StatelessWidget {
   final String label;
-  final String count;
+  final int count;
   final VoidCallback? onPressed;
 
-  const CategoryAnchor({
-    super.key,
-    required this.label,
-    required this.count,
-    this.onPressed,
-  });
+  const CategoryAnchor({super.key, required this.label, required this.count, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-
-    return TextButton(
+    return TextButton.icon(
       onPressed: onPressed,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            "$label ($count)",
-            style: theme.textTheme.labelLarge?.copyWith(
-              color: AppColors.lagoon,
-            ),
-          ),
-          const SizedBox(width: 8),
-          const DoomscrllIcon(
-            DoomscrllIconName.arrowForward,
-            size: 16,
-            color: AppColors.lagoon,
-          ),
-        ],
-      ),
+      label: Text("$label ($count)".upper, style: theme.textTheme.labelLarge?.copyWith(color: AppColors.lagoon)),
+      icon: const DoomscrllIcon(DoomscrllIconName.arrowForward, size: 16, color: AppColors.lagoon),
+      iconAlignment: IconAlignment.end,
     );
   }
 }
