9c0683f87606025df460f05d9142fbe9e2d708f3
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-18 00:55:48 +0300

    refactor: Update `CategoryAnchor` to use localized category labels

--- lib/views/home/widgets/category_anchor.dart
+++ lib/views/home/widgets/category_anchor.dart
@@ -1,21 +1,25 @@
+import "package:doomscrll_app_audience/l10n/category_l10n_extension.dart";
+import "package:doomscrll_app_audience/l10n/dict_extension.dart";
 import "package:doomscrll_app_audience/theme/app_colors.dart";
 import "package:doomscrll_app_audience/utils/string_extensions.dart";
 import "package:doomscrll_app_audience/views/common/doomscrll_icon.dart";
 import "package:flutter/material.dart";
 
 class CategoryAnchor extends StatelessWidget {
-  final String label;
+  final String category;
   final int count;
   final VoidCallback? onPressed;
 
-  const CategoryAnchor({super.key, required this.label, required this.count, this.onPressed});
+  const CategoryAnchor({super.key, required this.category, required this.count, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final categoryLabel = context.dict.getCategoryLabel(category);
+
     return TextButton.icon(
       onPressed: onPressed,
-      label: Text("$label ($count)".upper, style: theme.textTheme.labelLarge),
+      label: Text("$categoryLabel ($count)".upper, style: theme.textTheme.labelLarge),
       icon: const DoomscrllIcon(DoomscrllIconName.arrowForward, size: 16, color: AppColors.lagoon),
       iconAlignment: IconAlignment.end,
     );
