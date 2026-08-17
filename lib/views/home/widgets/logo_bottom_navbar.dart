574dec73cd31764826cd736fb8d376c77a86528e
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-17 22:31:39 +0300

    refactor: Add a callback to show `ContextSheet` as a modal bottom sheet to the button in `LogoBottomNavbar`

--- lib/views/home/widgets/logo_bottom_navbar.dart
+++ lib/views/home/widgets/logo_bottom_navbar.dart
@@ -1,9 +1,22 @@
 import "package:doomscrll_app_audience/theme/app_colors.dart";
+import "package:doomscrll_app_audience/views/common/doomscrll_icon.dart";
 import "package:doomscrll_app_audience/views/common/doomscrll_logo.dart";
+import "package:doomscrll_app_audience/views/common/context_sheet.dart";
 import "package:flutter/material.dart";
 
 class LogoBottomNavbar extends StatelessWidget {
   const LogoBottomNavbar({super.key});
+
+  void _showContextMenu(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      isScrollControlled: true,
+      showDragHandle: true,
+      backgroundColor: AppColors.darkness,
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(24.0))),
+      builder: (context) => const ContextSheet(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +31,8 @@ class LogoBottomNavbar extends StatelessWidget {
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: IconButton(
-                    onPressed: () => {},
-                    icon: const Icon(Icons.menu),
+                    onPressed: () => _showContextMenu(context),
+                    icon: const DoomscrllIcon(DoomscrllIconName.hamburger, color: AppColors.lagoon, size: 24),
                   ),
                 ),
               ),
