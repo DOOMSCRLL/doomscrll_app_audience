0227685f375d1a7c8d3294a1c4aee345912d535d
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-17 22:32:45 +0300

    refactor: Add a listener for locale changes under root `MaterialApp` widget in `main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import "package:wakelock_plus/wakelock_plus.dart";
 
 import "package:doomscrll_app_audience/l10n/app_localizations.dart";
 import "package:doomscrll_app_audience/theme/app_theme.dart";
+import "package:doomscrll_app_audience/viewmodels/locale_viewmodel.dart";
 import "package:doomscrll_app_audience/views/home/home_screen.dart";
 
 void main() {
@@ -16,12 +17,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "DOOMSCRLL",
-      theme: AppTheme.darkTheme,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const HomeScreen(),
+    return ListenableBuilder(
+      listenable: localeViewModel,
+      builder: (context, _) {
+        return MaterialApp(
+          title: "DOOMSCRLL",
+          theme: AppTheme.darkTheme,
+          locale: localeViewModel.locale,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: const HomeScreen(),
+        );
+      },
     );
   }
 }
