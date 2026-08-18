9b38941f4c5fec147469c7a9972ad22d014d8b60
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-18 18:51:19 +0300

    feat/wip: Define FeedScreen, implement most of the features from audience webapp, except querying and project screen navigation

---
+++ lib/views/feed/feed_screen.dart
@@ -0,0 +1,128 @@
+import "package:doomscrll_app_audience/l10n/dict_extension.dart";
+import "package:doomscrll_app_audience/models/project_category_count.dart";
+import "package:doomscrll_app_audience/theme/app_colors.dart";
+import "package:doomscrll_app_audience/utils/string_extensions.dart";
+import "package:doomscrll_app_audience/viewmodels/feed_viewmodel.dart";
+import "package:doomscrll_app_audience/views/common/doomscrll_refresh_indicator.dart";
+import "package:doomscrll_app_audience/views/common/doomscrll_spinner.dart";
+import "package:doomscrll_app_audience/views/common/doomscrll_wavy_divider.dart";
+import "package:doomscrll_app_audience/views/feed/widgets/feed_appbar.dart";
+import "package:doomscrll_app_audience/views/feed/widgets/feed_bottom_navbar.dart";
+import "package:doomscrll_app_audience/views/feed/widgets/project_preview_card.dart";
+import "package:flutter/material.dart";
+
+class FeedScreen extends StatefulWidget {
+  final String initialCategory;
+  final String? tag;
+  final int totalProjectCount;
+  final List<ProjectCategoryCount> projectCounts;
+
+  const FeedScreen({
+    super.key,
+    required this.initialCategory,
+    this.tag,
+    this.totalProjectCount = 0,
+    this.projectCounts = const [],
+  });
+
+  static Route<void> route({
+    required String initialCategory,
+    String? tag,
+    int totalProjectCount = 0,
+    List<ProjectCategoryCount> projectCounts = const [],
+  }) {
+    return MaterialPageRoute(
+      builder: (context) => FeedScreen(
+        initialCategory: initialCategory,
+        tag: tag,
+        totalProjectCount: totalProjectCount,
+        projectCounts: projectCounts,
+      ),
+    );
+  }
+
+  @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  late final FeedViewModel _viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = FeedViewModel();
+    _viewModel.init(
+      initialCategory: widget.initialCategory,
+      tag: widget.tag,
+      totalCategoryCount: widget.totalProjectCount,
+      projectCounts: widget.projectCounts,
+    );
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
+  Widget _buildBottomNavbar() => ListenableBuilder(
+    listenable: _viewModel,
+    builder: (_, _) => FeedBottomNavbar(
+      currentPage: _viewModel.currentPage,
+      countPages: _viewModel.countPages,
+      onPreviousPage: _viewModel.hasPrevPage ? _viewModel.previousPage : null,
+      onNextPage: _viewModel.hasNextPage ? _viewModel.nextPage : null,
+      onFilterPressed: () {
+        /* TODO: Open QuerySheet */
+      },
+    ),
+  );
+
+  Widget _buildBody(BuildContext context) => switch ((
+    _viewModel.isLoading,
+    _viewModel.errorMessage,
+    _viewModel.previews.isEmpty,
+  )) {
+    (true, _, true) => const Center(child: DoomscrllSpinner(size: 48)),
+    (_, final String err, true) => Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(err, style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: AppColors.bloodmoon)),
+          const SizedBox(height: 16),
+          OutlinedButton(onPressed: _viewModel.fetchFeed, child: Text(context.dict.feedActionLabelRetry.upper)),
+        ],
+      ),
+    ),
+    (_, _, true) => Center(child: Text(context.dict.feedCopyNoProjects, style: Theme.of(context).textTheme.bodyLarge)),
+    _ => ListView.separated(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      itemCount: _viewModel.previews.length,
+      separatorBuilder: (_, _) =>
+          const Padding(padding: EdgeInsets.symmetric(vertical: 40), child: DoomscrllWavyDivider()),
+      itemBuilder: (context, index) {
+        final preview = _viewModel.previews[index];
+        return ProjectPreviewCard(
+          project: preview,
+          itemIndex: index,
+          onTap: () {
+            /* TODO: Navigate to ProjectDetailsScreen */
+          },
+        );
+      },
+    ),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const FeedAppbar(),
+      bottomNavigationBar: _buildBottomNavbar(),
+      body: DoomscrllRefreshIndicator(
+        onRefresh: _viewModel.fetchFeed,
+        child: ListenableBuilder(listenable: _viewModel, builder: (context, _) => _buildBody(context)),
+      ),
+    );
+  }
+}
