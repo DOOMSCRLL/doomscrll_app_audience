b3c6e867855825fea06b2a7c3a2d4c7a36e451df
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-21 01:42:04 +0300

    refactor: Add debug check to the WakelockPlus call

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
-import "package:flutter/material.dart";
-import "package:wakelock_plus/wakelock_plus.dart";
-
 import "package:doomscrll_app_audience/l10n/app_localizations.dart";
 import "package:doomscrll_app_audience/theme/app_theme.dart";
 import "package:doomscrll_app_audience/viewmodels/locale_viewmodel.dart";
 import "package:doomscrll_app_audience/views/home/home_screen.dart";
+import "package:flutter/foundation.dart";
+import "package:flutter/material.dart";
+import "package:wakelock_plus/wakelock_plus.dart";
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  WakelockPlus.enable();
+
+  if (kDebugMode) WakelockPlus.enable();
+
   runApp(const MyApp());
 }
 
@@ -32,4 +34,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
