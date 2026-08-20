e6eaf15df5ce6813c9d57e891b8d0b12cc6c6d65
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-21 01:23:35 +0300

    fix: Add static asset CDN, and related utils to `cdn_utils.dart`

--- lib/utils/cdn_utils.dart
+++ lib/utils/cdn_utils.dart
@@ -1,10 +1,19 @@
 const String publicCdnProjectBaseUrl = "https://cdn.doomscrll.com/";
+const String publicCdnStaticBaseUrl = "https://assets.doomscrll.com/";
 
 String? resolveCdnImagePath(String? path) {
   if (path == null || path.isEmpty) return null;
   if (path.startsWith("http://") || path.startsWith("https://")) return path;
 
   final cleanBase = publicCdnProjectBaseUrl.endsWith("/") ? publicCdnProjectBaseUrl : "$publicCdnProjectBaseUrl/";
+  final cleanPath = path.startsWith("/") ? path.substring(1) : path;
+  return "$cleanBase$cleanPath";
+}
+
+String resolveCdnStaticAssetPath(String path) {
+  if (path.startsWith("http://") || path.startsWith("https://")) return path;
+
+  final cleanBase = publicCdnStaticBaseUrl.endsWith("/") ? publicCdnStaticBaseUrl : "$publicCdnStaticBaseUrl/";
   final cleanPath = path.startsWith("/") ? path.substring(1) : path;
   return "$cleanBase$cleanPath";
 }
