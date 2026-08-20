35368dddf8358da9d14aa6dc7497e73161e0875e
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-21 02:02:04 +0300

    refactor: Remove  WakelockPlus package from main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@ import "package:doomscrll_app_audience/l10n/app_localizations.dart";
 import "package:doomscrll_app_audience/theme/app_theme.dart";
 import "package:doomscrll_app_audience/viewmodels/locale_viewmodel.dart";
 import "package:doomscrll_app_audience/views/home/home_screen.dart";
-import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
-import "package:wakelock_plus/wakelock_plus.dart";
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (kDebugMode) WakelockPlus.enable();
+  //if (kDebugMode) WakelockPlus.enable();
 
   runApp(const MyApp());
 }
